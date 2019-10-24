b7ab21caeb5f3267ebe3b59804d3c7f0be2c1f8b
Frank Perri <fperri28@gmail.com>
2019-10-24 15:33:18 -0400

    updates

--- java/src/main/java/com/techelevator/reservation/JDBCReservationDAO.java
+++ java/src/main/java/com/techelevator/reservation/JDBCReservationDAO.java
@@ -1,11 +1,14 @@
 package com.techelevator.reservation;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
 
 public class JDBCReservationDAO implements ReservationDAO{
 
@@ -18,15 +21,25 @@ public class JDBCReservationDAO implements ReservationDAO{
 	
 	
 	@Override
-	public boolean addReservations(Reservation newReservation) {
+	public Reservation addReservations(Reservation newReservation) {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
 	}
 
 	@Override
 	public List<Reservation> getAllReservations() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Reservation> allReservations = new ArrayList<Reservation>();
+		
+		String sqlListAllReservationsQuery = 	"SELECT * " + 
+												"FROM reservation " + 
+												"ORDER BY name ASC";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlListAllReservationsQuery);
+		
+		while(results.next()) {	
+			Reservation aReservation = mapRowToReservation(results);
+			allReservations.add(aReservation);
+		}
+		return allReservations;
 	}
 
 	@Override
@@ -37,6 +50,12 @@ public class JDBCReservationDAO implements ReservationDAO{
 
 	@Override
 	public List<Reservation> getReservationsBySite(Long SiteId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Reservation getReservationById(int aReservationId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -57,6 +76,30 @@ public class JDBCReservationDAO implements ReservationDAO{
 	public boolean delReservation(Long resID) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+//	-------------------------------	HELPER METHODS	---------------------------------
+
+	private int getNextReservationId() {
+		SqlRowSet nextIdResult = jdbcTemplate.queryForRowSet("SELECT nextval('seq_reservation_id')");
+		if (nextIdResult.next()) {
+			return nextIdResult.getInt(1);
+		} else {
+			throw new RuntimeException("Something went wrong while getting an id for the new reservation");
+		}
+	}
+	
+	private Reservation mapRowToReservation(SqlRowSet results) {
+		Reservation theReservation = new Reservation(); 
+		
+		theReservation.setReservation_id(results.getInt("reservation_id"));
+		theReservation.setSite_id(results.getInt("site_id"));		
+		theReservation.setName(results.getString("name"));
+		theReservation.setFrom_date(results.getDate("from_date").toLocalDate());
+		theReservation.setTo_date(results.getDate("to_date").toLocalDate());
+		theReservation.setCreate_date(results.getDate("create_date").toLocalDate());
+		
+		return theReservation;
 	}
 
 }

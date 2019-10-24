fe430f9fa484ba11ad118c7a6874bd0a5f23af74
Beach <>
2019-10-24 16:14:52 -0400

    Reservations menu add

--- java/src/main/java/com/techelevator/CampgroundCLI.java
+++ java/src/main/java/com/techelevator/CampgroundCLI.java
@@ -30,25 +30,16 @@ public class CampgroundCLI {
 
 
 
-	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Parks";
-	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String MAIN_MENU_OPTION_EXIT = "Quit";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE,
-			MAIN_MENU_OPTION_EXIT };
-
 	private static final String VIEW_CAMPGROUNDS = "View Campgrounds";
 	private static final String SEARCH_RESERVATIONS = "Search for Reservations";
 	private static final String RETURN_TO_MAIN_MENU = "Return to Previous Screen";
-
 	private static final String[] SUB_MENU_OPTIONS = { VIEW_CAMPGROUNDS, SEARCH_RESERVATIONS,
-			RETURN_TO_MAIN_MENU };
+														RETURN_TO_MAIN_MENU };
 
-	private static final String SUB_SUB_MENU_ONE_DOLLAR = "Add $1.00";
-	private static final String SUB_SUB_MENU_TWO_DOLLARS = "Add $2.00";
-	private static final String	SUB_SUB_MENU_EXIT		= 	"EXIT";
-	private static final String[] SUB_SUB_MENU_OPTIONS = { SUB_SUB_MENU_ONE_DOLLAR, 
-															SUB_SUB_MENU_TWO_DOLLARS,
-															SUB_SUB_MENU_EXIT};
+	private static final String SEARCH_FOR_AVAILABLE_RESERVATIONS = "Search for Available Reservations";
+	private static final String	MENU_EXIT		= 					"Return to Previous Screen";
+	private static final String[] RESERVATION_MENU_OPTIONS = { SEARCH_FOR_AVAILABLE_RESERVATIONS, 
+															MENU_EXIT};
 
 	
 	
@@ -58,7 +49,7 @@ public class CampgroundCLI {
 	private CampgroundDAO campDAO;
 	private ReservationDAO resDAO;
 	private SiteDAO siteDAO;
-	
+	private String prevPark;
 	public static void main(String[] args) {
 		CampgroundCLI application = new CampgroundCLI();
 		application.run();
@@ -95,6 +86,7 @@ public class CampgroundCLI {
 					endMethodProcessing();
 				} else {
 					displayParkDetails(choice);
+					prevPark = choice;
 					campgrounds(choice);
 				}
 	}
@@ -105,57 +97,36 @@ public class CampgroundCLI {
 	 * Display the Sub menu and process option chosen
 	 ***************************************************************************************************************************/
 
-	public void campgrounds() {
-
-		boolean shouldProcess = true;
-		while (shouldProcess) {
+	public void campgrounds(String park) {
 
 			String choice = (String) campgroundMenu.getChoiceFromOptions(SUB_MENU_OPTIONS); 
-																									
-			switch (choice) {
 
-			case VIEW_CAMPGROUNDS:
-				
-				break;
-
-			case SEARCH_RESERVATIONS:
-				break;
-
-			case RETURN_TO_MAIN_MENU:
-				shouldProcess = false;
-				break;
-
+			if(choice.equals(VIEW_CAMPGROUNDS)){
+				System.out.println("Park Campgrounds");
+				displayCampgrounds(park);
+			} else if(choice.equals(SEARCH_RESERVATIONS)) {
+				Reservations();
+			} else if(choice.equals(RETURN_TO_MAIN_MENU)) {
+				run();
 			}
 		}
-		return;
-	}
 
 	/**************************************************************************************************************************
-	 * CampgroundCLI Feed money loop
+	 * CampgroundCLI loop
 	 * 
 	 * Display the sub menu and process option chosen
 	 ***************************************************************************************************************************/
 
-//	public void money() {
-//
-//
-//		String choice = (String) campgroundMenu.getChoiceFromOptions(SUB_SUB_MENU_OPTIONS); 
-//
-//		switch (choice) {
-//
-//		case SUB_SUB_MENU_ONE_DOLLAR:
-//			money();
-//			break;
-//
-//		case SUB_SUB_MENU_TWO_DOLLARS:
-//			money();
-//			break;
-//
-//		case SUB_SUB_MENU_EXIT:
-//			break;
-//		}
-//		return;
-//	}
+	public void Reservations() {
+
+		String choice = (String) campgroundMenu.getChoiceFromOptions(RESERVATION_MENU_OPTIONS); 
+
+		if(choice.equals(SEARCH_FOR_AVAILABLE_RESERVATIONS)){
+
+		} else if(choice.equals(MENU_EXIT)) {
+			campgrounds(prevPark);
+		}
+	}
 
 	/********************************************************************************************************
 	 * Methods used to perform processing
@@ -178,14 +149,31 @@ public class CampgroundCLI {
 		return parkNames.toArray();
 	}
 	
-	public Object[] displayCampgrounds(String parkId) {
+	public void displayCampgrounds(String parkName) {
+		List<Park> parksDetails = parkDAO.getParkByName(parkName);
+		int parkId = parksDetails.get(0).getPark_id();
+		
 		List<Campground> campgroundsByPark = campDAO.getCampgroundByPark(parkId);
+		System.out.printf(String.format("%-4s", ""));
+		System.out.printf(String.format("%-15s", "Name")); 
+		System.out.printf(String.format("%-20s", "Open"));
+		System.out.printf(String.format("%-25s", "Close"));
+		System.out.printf(String.format("%-30s", "Daily Fee"));
+		System.out.println();
+
+		if(campgroundsByPark.size() > 0) {
+			for(Campground cur : campgroundsByPark) {
+				System.out.printf(String.format("%-4s", cur.getCampground_id()));
+				System.out.printf(String.format("%-15s", cur.getName()));
+				System.out.printf(String.format( "%-20s", cur.getOpen_from_mm()));
+				System.out.printf(String.format( "%-25s", cur.getOpen_to_mm()));
+				System.out.printf(String.format( "%-30s", cur.getDaily_fee()));
+				System.out.println();
+			}
+		} else {
+			System.out.println("\n*** No results ***");
+		}
 		
-		
-		
-		
-		
-		return ;
 	}
 
 
